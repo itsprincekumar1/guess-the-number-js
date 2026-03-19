e4ce36174cb8ee10c5c83192e36e99c32376879b
itsprincekumar1 <itsprincekumar19@gmail.com>
2026-03-19 19:33:39 +0530

    Complete game logic and design

--- script.js
+++ script.js
@@ -2,23 +2,40 @@ const min = 1;
 const max = 20;
 
 let number = Math.floor(Math.random()*(max-min)+min);
-let running = true;
-let attempt = 0;
+let attempt = 5;
 let guess;
 
-while(running){
-    guess = window.prompt(`Enter a number between ${min}-${max}.`);
+const submit = document.getElementById("user-submit");
+const resetbtn = document.getElementById("reset-btn");
+let lifes = document.getElementById("lifes-left").textContent;
+document.getElementById("lifes-left").textContent = lifes.repeat(attempt);
+
+submit.onclick = function(){
+    guess = document.getElementById("user-input").value;
     if(isNaN(guess) || guess<min ||guess>max){
-        window.alert("Enter a valid number");
+        document.getElementById("result").textContent = "Enter a valid number."
     }else if(guess<number){
-        attempt++;
-        window.alert("Too low, try a higher number.");
+        attempt--;
+        document.getElementById("lifes-left").textContent = lifes.repeat(attempt);
+        document.getElementById("result").textContent = "Too Low, try a higher number.";
     }else if(guess>number){
-        attempt++;
-        window.alert("Too high, try a lower number.");
+        attempt--;
+        document.getElementById("lifes-left").textContent = lifes.repeat(attempt);
+        document.getElementById("result").textContent = "Too High, try a lower number.";
     }else{
-        attempt++;
-        window.alert(`CORRECT! You took ${attempt} attempts to guess ${number}`);
-        running=false;
+        document.getElementById("result").textContent = `CORRECT! Your score is ${attempt*100}/500.`;
     }
+    if(attempt==0){
+        document.getElementById("result").textContent = "GAME OVER YOU LOSE";
+        return;
+    }
+}
+
+resetbtn.onclick = function(){
+    number = Math.floor(Math.random()*(max-min)+min);
+    console.log(number);
+    document.getElementById("result").textContent = "Enter your number.";
+    attempt = 5;
+    document.getElementById("user-input").value = "";
+    document.getElementById("lifes-left").textContent = lifes.repeat(attempt);
 }
